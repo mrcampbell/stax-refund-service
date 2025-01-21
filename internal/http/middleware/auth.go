0d4baaea64e6f4b7ceee8d1c3586f6c883e03db1
Mike Campbell <michaelrcampbell0916@gmail.com>
2025-01-21 09:47:29 -0700

    added comment in auth middleware to actually use the service

--- internal/http/middleware/auth.go
+++ internal/http/middleware/auth.go
@@ -15,6 +15,8 @@ const AuthUserIDContextKey = "authenticated-user-id"
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Request.Header.Get("Authorization")
+
+		// todo: actually use the auth service, even just the stubbed one
 		config.PanicIfNotDev() // because we have stubbed auth
 		if token != fmt.Sprintf("Bearer %s", mock.MockStubbedAuthToken()) {
 			c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
